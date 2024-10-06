beb5888c0dbde4d7c45c9ed34bb3edcff44bc25d
Mufrid Shovon <mia.md.mufrid@gmail.com>
2024-10-06 10:58:06 +0600

    updated template

--- App_Hotel/admin.py
+++ App_Hotel/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from App_Hotel import models
+
+
+admin.site.register(models.Location)
+admin.site.register(models.Amenity)
+admin.site.register(models.Hotel)
+admin.site.register(models.Room)
+admin.site.register(models.Booking)
+admin.site.register(models.Payment)
